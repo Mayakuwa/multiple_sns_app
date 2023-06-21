61724e877d29ce8439c7413e5738714e13e54d10
Maya <mayakuwano55@gmail.com>
2023-06-21 21:43:49 +0900

    freezed追加

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -3,20 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:uuid/uuid.dart';
+import 'package:first_app/domain/firestore_user/firestore_user.dart';
 
 // ViewとModelを橋渡ししてくれるよ
 final mainProvider = ChangeNotifierProvider((ref) => MainModel());
 
 class MainModel extends ChangeNotifier {
-  int counter = 0;
+  int counter = 1;
 
 // setStateはstatefulwidgetでしか使えないよ。変更を知らせるにはnotifyListenersを使う
   Future<void> createUser({required BuildContext context}) async {
     counter++;
     final uuid = Uuid();
     final String v4 = uuid.v4();
-    final Map<String, dynamic> userDate = {'userName': 'Alice', 'uid': v4};
-    await FirebaseFirestore.instance.collection('users').doc(v4).set(userDate);
+    // final Map<String, dynamic> userDate = {'userName': 'Alice', 'uid': v4};
+    final FirestoreUser firestoreUser = FirestoreUser(uid: v4, userName: 'Alice');
+    final Map<String, dynamic> userData = firestoreUser.toJson();
+
+    await FirebaseFirestore.instance.collection('users').doc(v4).set(userData);
+
     ScaffoldMessenger.of(context)
         .showSnackBar(SnackBar(content: Text('ユーザが作成されたよ')));
     notifyListeners();
